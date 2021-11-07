16fb068fa772f1e2bb9fccbc28e92841ee9a690a
NomanSaleem504 <63067479+NomanSaleem504@users.noreply.github.com>
2021-11-07 01:56:12 -0800

    JavaScript
    JavaScript

---
+++ index.js
@@ -0,0 +1,9 @@
+//let text = "Please visit Microsoft!";
+//let newText = text.replace ("Microsoft", "Pakistan");
+//document.write(newText);
+
+
+function myFunction() {
+    let text = document.write(newText); 
+    text.replace(/MICROSOFT/i,"Saudi Arab");
+}
